2c4bbf55e965dedd7e1e78011673cacfba96776c
GitHub Action <action@github.com>
2025-04-16 19:15:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/raystatic/commitgenie/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/raystatic/commitgenie/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.raystatic.commitgenie.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
